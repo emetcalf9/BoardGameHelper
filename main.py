b3440260a6936d92882ea420be443c1f97b59c4a
Eric Metcalf <63564633+emetcalf9@users.noreply.github.com>
2020-10-03 17:11:33 -0700

    Add files via upload
    Fixed issue with choice variable not being converted to int

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ while True:
     4. List games
     5. Reset the list
     6. Quit""")
-    if choice == 1:
+    if int(choice) == 1:
         play_a_game()
-    elif choice == 2:
+    elif int(choice) == 2:
         name = input("Enter the name of the game")
         minPlay = input("Enter the minimum number of players")
         maxPlay = input("Enter the maximum number of players")
         difficulty = input("Enter the difficulty (1-4)")
         add_game(name,minPlay,maxPlay,difficulty)
-    elif choice == 3:
+    elif int(choice) == 3:
         name = input("Which game would you like to remove?")
         remove_game(name)
-    elif choice == 4:
+    elif int(choice) == 4:
         print("This function has not been implemented yet")
-    elif choice == 5:
+    elif int(choice) == 5:
         confirm = input('Are you completely sure? Type "Yes" to continue')
         if confirm == 'Yes':
             reset_list()
-    elif choice == 6:
+    elif int(choice) == 6:
         print("Bye for now!")
         break
